3f9465299bef8b09ea2b772dd24077f0e8f1b037
adthoughtsglobal <adthoughtsglobal@gmail.com>
2025-02-01 12:50:43 +0530

    updates

--- script.js
+++ script.js
@@ -1525,7 +1525,7 @@ function displayNotifications(x) {
 }
 function runAsOSL(content) {
 	const encodedContent = encodeURIComponent(content).replace(/'/g, "%27").replace(/"/g, "%22");
-	const cont = `<iframe class="oslframe" src="https://origin.mistium.com/Versions/originv4.9.2.html?embed=${encodedContent}"></iframe>
+	const cont = `<iframe class="oslframe" src="https://origin.mistium.com/Versions/originv5.5.1.html?embed=${encodedContent}"></iframe>
 	<style>
 		.oslframe {
 			width: 100%;
