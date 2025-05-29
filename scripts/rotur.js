35c763868b6a009d9b44af74bbd12e8ade5bda40
adthoughtsglobal <123654929+adthoughtsglobal@users.noreply.github.com>
2025-05-29 20:01:07 +0530

    updates and payment app

--- scripts/rotur.js
+++ scripts/rotur.js
@@ -1816,7 +1816,7 @@ class RoturExtension {
     return this.user["sys.currency"] ?? 0;
   }
 
-  tranferCurrency(args) {
+  transferCurrency(args) {
     if (!this.is_connected) {
       return "Not Connected";
     }
