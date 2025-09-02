e2216bb5d991e4c4599a7ddad2bc1ba7160106a0
adthoughtsglobal <123654929+adthoughtsglobal@users.noreply.github.com>
2025-09-02 16:11:34 +0530

    camera app filters and more

--- script.js
+++ script.js
@@ -232,7 +232,7 @@ async function startup() {
 						}
 					}
 					await setSetting("versions", lclver, "defaultApps.json")
-					if (howmany) toast( howmany+ " default app(s) have been updated")
+					if (howmany) toast(howmany + " default app(s) have been updated")
 				} else {
 					console.error("Failed to fetch data from the server.");
 				}
@@ -547,58 +547,58 @@ function getAppAspectRatio(unshrunkContent) {
 }
 async function getAppIcon(content, id, lazy = 0) {
 	try {
-if (content, id == undefined) {
-		if (content == 'folder')
-			return `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="var(--col-txt1)"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h207q16 0 30.5 6t25.5 17l57 57h360q17 0 28.5 11.5T880-680q0 17-11.5 28.5T840-640H447l-80-80H160v480l79-263q8-26 29.5-41.5T316-560h516q41 0 64.5 32.5T909-457l-72 240q-8 26-29.5 41.5T760-160H160Zm84-80h516l72-240H316l-72 240Zm-84-262v-218 218Zm84 262 72-240-72 240Z"/></svg>`;
+		if (content, id == undefined) {
+			if (content == 'folder')
+				return `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="var(--col-txt1)"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h207q16 0 30.5 6t25.5 17l57 57h360q17 0 28.5 11.5T880-680q0 17-11.5 28.5T840-640H447l-80-80H160v480l79-263q8-26 29.5-41.5T316-560h516q41 0 64.5 32.5T909-457l-72 240q-8 26-29.5 41.5T760-160H160Zm84-80h516l72-240H316l-72 240Zm-84-262v-218 218Zm84 262 72-240-72 240Z"/></svg>`;
 
-		return defaultAppIcon;
-	} else if (id == "info") {
-		return `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="var(--col-txt1)"><path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>`
-	}
-	const withTimeout = (promise) =>
-		Promise.race([promise, new Promise((_, reject) => setTimeout(() => reject(), 3000))]);
-
-	const getAppIconFromRegistry = async (id, registry) => {
-		if (registry && registry.icon) {
-			appicns = registry.icon;
-			return appicns;
+			return defaultAppIcon;
+		} else if (id == "info") {
+			return `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="var(--col-txt1)"><path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>`
 		}
-		return null;
-	};
+		const withTimeout = (promise) =>
+			Promise.race([promise, new Promise((_, reject) => setTimeout(() => reject(), 3000))]);
 
-	const saveIconToRegistry = async (id, iconContent, registry) => {
-		const updatedRegistry = {
-			...(registry || {}),
-			icon: iconContent
+		const getAppIconFromRegistry = async (id, registry) => {
+			if (registry && registry.icon) {
+				appicns = registry.icon;
+				return appicns;
+			}
+			return null;
 		};
-		await setSetting(id, updatedRegistry, "AppRegistry.json");
-	};
 
-	try {
-		if (appicns[id]) return appicns[id];
-		if (lazy) return defaultAppIcon;
+		const saveIconToRegistry = async (id, iconContent, registry) => {
+			const updatedRegistry = {
+				...(registry || {}),
+				icon: iconContent
+			};
+			await setSetting(id, updatedRegistry, "AppRegistry.json");
+		};
 
-		const registry = await getSetting(id, "AppRegistry.json") || {};
-		const cachedIcon = await getAppIconFromRegistry(id, registry);
-		if (cachedIcon) return cachedIcon;
+		try {
+			if (appicns[id]) return appicns[id];
+			if (lazy) return defaultAppIcon;
 
-		if (!content) {
-			const file = await withTimeout(await getFileById(id));
-			if (!file || !file.content) throw new Error("File content unavailable " + id);
-			content = file.content;
+			const registry = await getSetting(id, "AppRegistry.json") || {};
+			const cachedIcon = await getAppIconFromRegistry(id, registry);
+			if (cachedIcon) return cachedIcon;
+
+			if (!content) {
+				const file = await withTimeout(await getFileById(id));
+				if (!file || !file.content) throw new Error("File content unavailable " + id);
+				content = file.content;
+			}
+			const iconContent = await withTimeout(getMetaTagContent(content, 'nova-icon', true));
+			if (iconContent && containsSmallSVGElement(iconContent)) {
+				appicns[id] = iconContent;
+				await saveIconToRegistry(id, iconContent, registry);
+				return iconContent;
+			}
+		} catch (err) {
+			console.error("Error in getAppIcon:", err);
 		}
-		const iconContent = await withTimeout(getMetaTagContent(content, 'nova-icon', true));
-		if (iconContent && containsSmallSVGElement(iconContent)) {
-			appicns[id] = iconContent;
-			await saveIconToRegistry(id, iconContent, registry);
-			return iconContent;
-		}
-	} catch (err) {
-		console.error("Error in getAppIcon:", err);
-	}
 
-	} catch (e) {}
-	
+	} catch (e) { }
+
 	const fallbackIcon = generateFallbackIcon(id);
 	appicns[id] = fallbackIcon;
 
@@ -911,7 +911,7 @@ async function registerApp(appId, capabilities) {
 	}
 
 	if (!initialization)
-	notify(await getFileNameByID(appId) + " installed", "Registered " + capabilities.toString(), "NovaOS System");
+		notify(await getFileNameByID(appId) + " installed", "Registered " + capabilities.toString(), "NovaOS System");
 	return capabilities.toString();
 }
 
@@ -1252,10 +1252,13 @@ async function initializeOS() {
 async function installdefaultapps() {
 	nonotif = true;
 	gid("edison").showModal();
+	gid("lazarus").showModal()
 	if (gid('startupterms')) {
 		gid('startupterms').innerText = "Just a moment...";
 	}
 	gid("appdmod").close();
+	setTimeout(() => gid("lazarus").classList.add("closeEffect"), 2700);
+	setTimeout(() => gid("lazarus").close(), 3000);
 
 	const maxRetries = 3;
 	const failedApps = [];
